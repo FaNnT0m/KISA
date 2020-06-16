534b13eeeafacf50ac98c3290ec1d96b8349fe52
scamposr020 <scamposr020@ulacit.ed.cr>
2020-06-16 17:36:23 -0600

    Changes requested by ALEK had been added

--- apps/main/models.py
+++ apps/main/models.py
@@ -1,7 +1,42 @@
 from django.db import models
 from django.contrib.auth.models import User
-
+from django.utils import timezone
 # Create your models here.
+
+PROVINCES = (
+    ("1", "San Jose"),
+    ("2", "Alajuela"),
+    ("3", "Cartago"),
+    ("4", "Heredia"),
+    ("5", "Guanacaste"),
+    ("6", "Puntarenas"),
+    ("7", "Limon")
+)
+
+DISTRICTS = (
+    ("1","Carmen"), 
+    ("2","Merced"),
+    ("3", "Hospital"),
+    ("4","San Antonio"),
+    ("5","Sabanilla"),
+    ("6", "Palmares"),
+    ("7","San Nicolás"),
+    ("8","Tierra Blanca"),
+    ("9","Orosi"),
+    ("10","Mercedes"),
+    ("11","Barva"),
+    ("12", "San Domingo")
+    ("13","Liberia"),
+    ("14","Nicoya"),
+    ("15", "Sámara"),
+    ("16","Paquera"),
+    ("17","Cóbano"),
+    ("18","Guacimal"),
+    ("19","Guápiles"),
+    ("20","Siquirres"),
+    ("21", "La Rita")
+)
+
 class Person(BaseModel):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     birth_date = models.DateField()
@@ -20,7 +55,7 @@ class Client(Person):
         pass
 
 class PaymentMethod(BaseModel):
-    client = models.OneToOneField(Client, on_delete=models.CASCADE)    
+    client = models.ForeignKey(Client, on_delete=models.CASCADE)    
     card_number = models.IntegerField()
     card_holder = models.CharField(max_length = 80)
     cv2 = models.IntegerField()
@@ -30,24 +65,24 @@ class Driver(Person):
     pass
 class BusRoute(BaseModel):
     title =  models.CharField(max_length = 80)
-    driver = models.OneToOneField(Driver, on_delete=models.CASCADE)
+    driver = models.ForeignKey(Driver, on_delete=models.CASCADE)
     ticket_price = models.FloatField()
     ctp_code = models.IntegerField()
-    province =  models.CharField(max_length = 30)
-    district =  models.CharField(max_length = 30)
+    province =  models.ChoiceField(province=PROVINCES)
+    district =  models.ChoiceField(district=DISTRICTS)
 
 
 class BusRouteTicket(BaseModel):
-    client = models.OneToOneField(Client, on_delete=models.CASCADE)    
-    bus_route = models.OneToOneField(BusRoute, on_delete=models.CASCADE)
+    client = models.ForeignKey(Client, on_delete=models.CASCADE)    
+    bus_route = models.ForeignKey(BusRoute, on_delete=models.CASCADE)
     amount_payed = models.FloatField()
 
 class BaseModel(models.Model):
-    created_date = models.DateTimeField()
+    created_date = models.DateTimeField(default=timezone.now())
     created_by = models.CharField(max_length = 30)
-    updated_date = models.DateTimeField()
+    updated_date = models.DateTimeField(default=timezone.now())
     updated_by = models.CharField(max_length = 30)
-    deleted_date = models.DateTimeField()
-    deleted_by = models.CharField(max_length = 30)
+    deleted_date = models.DateTimeField(db_index=True, null=True, blank=True)
+    deleted_by = models.CharField(max_length = 30 ,null=True, blank=True)
 
 
