70e93956842b0e1a6009866af26c448d8b3f2fa4
eviltwin666 <kcamposr021@ulacit.ed.cr>
2020-07-07 16:27:44 -0600

    added birth date field to admin panel

--- apps/main/admin.py
+++ apps/main/admin.py
@@ -2,10 +2,10 @@ from django.contrib import admin
 from .models import Driver
 
 class DriverAdmin(admin.ModelAdmin):
-    fields = ('user','last_login_date')
+    fields = ('user','last_login_date','birth_date')
 
 class DriverAdmin(admin.ModelAdmin):
-    exclude = ('birth_date','created_date','updated_date','deleted_date')
+    exclude = ('created_date','updated_date','deleted_date')
 
 admin.site.register(Driver,DriverAdmin)
 
