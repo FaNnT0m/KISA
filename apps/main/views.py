3be01020a4840104b46fecfba1f9f7b5a4bdd8c9
Alek Castillo <alek.castillo@smiledirectclub.com>
2020-08-21 20:52:16 -0600

    moved payment methods and added reports filtering

--- apps/main/views.py
+++ apps/main/views.py
@@ -48,13 +48,25 @@ def register(request):
 @group_required(CLIENT_GROUP_NAME)
 def digital_wallet(request):
     client = request.user.client
+
     if request.method == 'POST':
+        form = PaymentMethodForm(request.POST)
         balance_to_add = int(request.POST['balance_to_add'])
-        client.add_balance(balance_to_add)
-        client.save()
+        if form.is_valid():
+            payment_method = form.save(commit=False)
+            payment_method.client = client
+            payment_method.save()
+            client.add_balance(balance_to_add)
+            client.save()
+            messages.success(request, f'Transation succesful!')
+            return redirect('digital_wallet')
+
+    else:
+        form = PaymentMethodForm()
 
     context = {
-        'client': client
+        'client': client,
+        'form' : form,
     }
     return render(request, 'main/digital_wallet.html', context)
 
@@ -66,8 +78,10 @@ def client_reports(request):
     tickets = ""
     if request.method == 'POST':
         year = request.POST['years']
-        month =request.POST['months']
-        tickets = BusRouteTicket.objects.all().filter(client_id=client.id, payment_successful=True).filter(created_date__month=month,created_date__year=year)
+        month = request.POST['months']
+        tickets = BusRouteTicket.objects.all().filter(client_id=client.id, payment_successful=True, created_date__month=month, created_date__year=year)
+        if tickets.count < 1:
+            messages.info(request, f'You had no trips in the selected time period')
 
     context = {
         'client': client,
@@ -80,6 +94,7 @@ def client_reports(request):
 @group_required(DRIVER_GROUP_NAME)
 def driver_route(request):
     driver = request.user.driver
+    tickets = BusRouteTicket.objects.all().filter(driver_id=driver.id)
     if request.method == 'POST':
         client_identification = request.POST['client_identification']
         client = Client.objects.all().filter(identification=client_identification).first()
@@ -91,26 +106,14 @@ def driver_route(request):
             else:
                 messages.error(request, f'Client had insuficient funds!')
 
-    tickets = BusRouteTicket.objects.all().filter(driver_id=driver.id)
+    if 'date' in request.GET:
+        date = request.GET['date']
+        tickets = tickets.filter(created_date__date=date)
+        if tickets.count() < 1:
+            messages.info(request, f'You had no passengers in the selected day')
 
     context = {
         'driver': driver,
         'tickets': list(tickets)
     }
     return render(request, 'main/driver_route.html', context)
-
-@group_required(CLIENT_GROUP_NAME)
-def payment_methods(request):
-    if request.method == 'POST':
-        form = PaymentMethodForm(request.POST)
-        if form.is_valid():
-            payment_method = form.save(commit=False)
-            payment_method.client = request.user.client
-            payment_method.save()
-            messages.success(request, f'Card added succesfully')
-            return redirect('payment_methods')
-
-    else:
-        form = PaymentMethodForm()
-
-    return render(request, 'main/payment_methods.html',{'form':form})
